d765fef24d7bf1c53bc8ef7361a9b12215f95752
Rebecca Lin <rlin178@gmail.com>
2023-04-12 00:10:14 -0400

    added all controllers

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import AdminController from "./controllers/admin/admin-controller.js";
+import ReviewController from "./controllers/reviews/review-controller.js";
 import UserController from "./controllers/users/user-controller.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+const CONNECTION_STRING = "fill in the blank here";
+mongoose.connect(CONNECTION_STRING);
+
+AdminController(app);
+ReviewController(app);
 UserController(app);
+app.get('/', (req, res) => {
+    res.send('Life is good!')});
+
+
 app.listen(4000);
