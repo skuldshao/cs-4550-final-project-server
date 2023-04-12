a50337d9b990246acdaa9d2280875f6640687c2b
Rebecca Lin <rlin178@gmail.com>
2023-04-11 23:29:27 -0400

    added cors and mongoose

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-const express = require('express')
-const app = express()
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
 app.get('/hello', (req, res) => {res.send('Life is good!')})
 app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')})
 app.listen(4000)
