56d27cd63b39cff20a6c150c2e7f3e70003b9a2a
Rebecca Lin <rlin178@gmail.com>
2023-04-18 14:23:18 -0400

    added express-session and updated controller with emailpassword

--- app.js
+++ app.js
@@ -4,11 +4,25 @@ import mongoose from "mongoose";
 import AdminController from "./controllers/admin/admin-controller.js";
 import ReviewController from "./controllers/reviews/review-controller.js";
 import UserController from "./controllers/users/user-controller.js";
+import session from "express-session";
 
 const app = express();
-app.use(cors());
+app.use(
+    session({
+        secret: "any string",
+        resave: false,
+        saveUninitialized: true,
+    })
+);
+app.use(
+    cors({
+        credentials: true,
+        origin: "http://localhost:3000",
+    })
+);
 app.use(express.json());
 
+const port = process.env.PORT || 4000;
 const CONNECTION_STRING = "mongodb://127.0.0.1:27017/goodsounds";
 mongoose.connect(CONNECTION_STRING);
 
@@ -18,4 +32,4 @@ UserController(app);
 app.get('/', (req, res) => {
     res.send('GoodSounds API')});
 
-app.listen(4000);
+app.listen(port);
--- controllers/users/user-dao.js
+++ controllers/users/user-dao.js
@@ -1,6 +1,7 @@
 import userModel from "./user-model.js";
 export const findUsers = () => userModel.find();
 export const findUserById = (userId) => userModel.findOne({_id: userId});
+export const findUserByEmailPassword = (email, password) => userModel.findOne({email: email, password: password});
 export const createUser = (user) => userModel.create(user);
 export const deleteUser = (userId) => userModel.deleteOne({_id: userId});
 export const updateUser = (userId, user) => userModel.updateOne({_id: userId}, {$set: user})
