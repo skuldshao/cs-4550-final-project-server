006fe17192ce1c2dfc94d1e728c409f3f7dc3d75
Rebecca Lin <rlin178@gmail.com>
2023-04-18 14:25:03 -0400

    added admin emailpassword

--- controllers/admin/admin-dao.js
+++ controllers/admin/admin-dao.js
@@ -1,6 +1,7 @@
 import adminModel from "./admin-model.js";
 export const findAdmins = () => adminModel.find();
 export const findAdminById = (adminId) => adminModel.findOne({_id: adminId});
+export const findAdminByEmailPassword = (email, password) => adminModel.findOne({email: email, password: password});
 export const createAdmin = (admin) => adminModel.create(admin);
 export const deleteAdmin = (adminId) => adminModel.deleteOne({_id: adminId});
 export const updateAdmin = (adminId, admin) => adminModel.updateOne({_id: adminId}, {$set: admin})
